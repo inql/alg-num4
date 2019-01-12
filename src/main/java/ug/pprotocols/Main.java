e46ac6d41adc2419e8aad3af4cf9894823236c03
Mateusz Małecki <mmateusz314@gmail.com>
2019-01-12 18:32:50 +0100

    Avg approximation function added

--- src/main/java/ug/pprotocols/Main.java
+++ src/main/java/ug/pprotocols/Main.java
@@ -47,7 +47,7 @@ public class Main {
 
         Map<Integer, Integer> testScope = new HashMap<Integer, Integer>() {{
         }};
-        for(int i =3; i<=20; i++){
+        for(int i =10; i<=30; i++){
             testScope.put(i,10);
         }
 
